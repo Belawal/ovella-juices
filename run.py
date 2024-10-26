3de71603e4abfb8a972017d9299cacbed33e6a5b
Belawal Khan <Belawal.khan@gmail.com>
2024-10-26 12:26:05 +0000

    add split set data , add validate functiuon, with raise error,

--- run.py
+++ run.py
@@ -16,10 +16,31 @@ def get_sales():
     """
     Get Daily sales data 
     """
+# message display for user's input prompt    
 print("Please enter the number of juices sold today")
 print("Data input shoud in sequence wise of Mango, Apple, Guava, Pome\n")
 
+# user input daily sales
 data_str = input("enter your daily sales here: ")
-print(f"The Sale are {data_str}")
+
+# daily sales display in set i.e "10", "5", "10", "20".
+sales_data = data_str.split(",")
+validate_data(sales_data)
+
+
+def validate_data(values):
+    """ 
+    checks Input value can be converted into integers,
+    if not then raises ValueError 
+    or the volume of value isn't in set of 4. 
+    """
+    try:
+    if len(values) != 4:
+        raise ValueError(
+            f"Exactly 4 values required, you provided {len(values)}"
+             )
+             except ValueError as e:
+    print(f"Invaid data: {e}, please enter the correct format.\n")
 
 get_sales()
+ 
